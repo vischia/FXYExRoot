927f4fcae89cb8dc4c9b896fa38c815bac300bc3
Pietro Vischia <pietro.vischia@gmail.com>
2016-05-01 16:35:50 +0100

    Added parallel run option to batcher

--- batcher.sh
+++ batcher.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+PARALLEL=${1}
+DOPAR=""
+if [ "${PARALLEL}" == "1" ]; then
+    DOPAR="&"
+fi
 for i in `find ../Rui_ilc/ -maxdepth 2 -type d -name 'run_*' | grep Events`
     do
-        python GenAnalysis.py -b -i ${i}
+        python GenAnalysis.py -b -i ${i} ${DOPAR}
     done
 
 for i in `find ../Rui_cepc/ -maxdepth 2 -type d -name 'run_*' | grep Events`
     do
-        python GenAnalysis.py -b -i ${i}
+        python GenAnalysis.py -b -i ${i} ${DOPAR}
     done
 exit 0
