7b672372f196ba2322252c4cdcc2a2767190f224
Pietro Vischia <pietro.vischia@gmail.com>
2016-05-02 09:56:34 +0100

    Add bad batcher that runs everything in parallel and hangs the machine

--- batcher.sh
+++ batcher.sh
@@ -2,16 +2,21 @@
 
 PARALLEL=${1}
 DOPAR=""
-if [ "${PARALLEL}" == "1" ]; then
+
+if [ ${PARALLEL} -eq 1 ]
+   then
     DOPAR="&"
 fi
+
+echo "DOPAR IS ${DOPAR}"
+
 for i in `find ../Rui_ilc/ -maxdepth 2 -type d -name 'run_*' | grep Events`
     do
-        python GenAnalysis.py -b -i ${i} ${DOPAR}
+        python GenAnalysis.py -b -i ${i} &
     done
 
 for i in `find ../Rui_cepc/ -maxdepth 2 -type d -name 'run_*' | grep Events`
     do
-        python GenAnalysis.py -b -i ${i} ${DOPAR}
+        python GenAnalysis.py -b -i ${i} &
     done
 exit 0
