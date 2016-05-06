ce179f25e3d3edcfef5bb90bb2d6499e6b2dd452
Pietro Vischia <pietro.vischia@gmail.com>
2016-05-06 19:12:15 +0100

    Added workflow list

---
+++ doStuff.sh
@@ -0,0 +1,11 @@
+python getXsecs.py -u file:///home/junzi/workarea/production/Production/MG5_aMC_v2_3_3/Rui_ilc_bb/crossx.html
+python getXsecs.py -u file:///home/junzi/workarea/production/Production/MG5_aMC_v2_3_3/Rui_cepc_bb/crossx.html
+
+
+python runBatch.py -c cepc_bb -n 8 -g
+python runBatch.py -c ilc_bb -n 8 -g
+
+python GenDisplay.py -o plots/ -g -b
+
+python plotXsec.py -b 
+
