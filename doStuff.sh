c8da210ca13a2605d4849589ceb0fe2d5c877e6c
Pietro Vischia <pietro.vischia@gmail.com>
2016-05-07 12:27:30 +0100

    Added gluon-gluon branching ratios

--- doStuff.sh
+++ doStuff.sh
@@ -1,6 +1,8 @@
 python getXsecs.py -u file:///home/junzi/workarea/production/Production/MG5_aMC_v2_3_3/Rui_ilc_bb/crossx.html
 python getXsecs.py -u file:///home/junzi/workarea/production/Production/MG5_aMC_v2_3_3/Rui_cepc_bb/crossx.html
 
+python getXsecs.py -g -u file:///home/junzi/workarea/production/Production/MG5_aMC_v2_3_3/Rui_ilc_gg/crossx.html
+python getXsecs.py -g -u file:///home/junzi/workarea/production/Production/MG5_aMC_v2_3_3/Rui_cepc_gg/crossx.html
 
 python runBatch.py -c cepc_bb -n 8 -g
 python runBatch.py -c ilc_bb -n 8 -g
--- getXsecs.py
+++ getXsecs.py
@@ -3,7 +3,7 @@ from BeautifulSoup import BeautifulSoup
 import optparse
 
 
-def printInfo(url):
+def parseInfo(url, gluons):
     ab = anonBrowser()
     ab.anonymize()
     page = ab.open(url)
@@ -17,8 +17,12 @@ def printInfo(url):
         returnDict = {}
         for i in xsecs:
             theref = i['href']
-            if theref.find('results.html') == -1 or theref.find('decayed') == -1 :
-                continue
+            if gluons:
+                if theref.find('results.html') == -1 or theref.find('run_') == -1 :
+                    continue
+            else:
+                if theref.find('results.html') == -1 or theref.find('decayed') == -1 :
+                    continue
             runCode = theref.split('_')[1]
             xsec = i.contents[0]
             unc  = i.contents[2]
@@ -33,15 +37,16 @@ def printInfo(url):
 def main():
     parser = optparse.OptionParser('Usage%prog ' + '-u <target url>')
     parser.add_option('-u', dest='targetURL', type='string', help='specify target ULR')
-
+    parser.add_option('-g', dest='gluons', action='store_true', help='run on hZ -> bb (h undecayed)')
     (options, args) = parser.parse_args()
 
     url = options.targetURL
+    gluons = options.gluons
     if url == None:
         print parser.usage
         exit(0)
     else:
-        resultDict = printInfo(url)
+        resultDict = parseInfo(url, gluons)
         # print resultDict
 if __name__ == '__main__':
     main()
